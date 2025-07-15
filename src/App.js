dc11dd9aa91601808f4821838f5b3efeb9fabcc4
Son Nguyen <trgson250411@gmail.com>
2025-07-15 22:47:39 +0700

    update: App.js & index.js + add css for components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
+import Home from './components/Home';
+import ProductDetail from './components/ProductDetail';
+import CartInfo from './components/CartInfo';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:productId" element={<ProductDetail />} />
+        <Route path="/cartInfo" element={<CartInfo />} />
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
   );
 }
 
